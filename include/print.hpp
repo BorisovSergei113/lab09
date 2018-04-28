00b889cd0101a7f520de5cde5819722ebb5ee70a
BorisovSergei113 <borisov_sergei113@mail.ru>
2018-04-28 15:14:40 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -1,5 +1,12 @@
+/** Print text in file and monitor */
 #include <string>
 #include <fstream>
 #include <iostream>
+/** @brief  Вывод текста на экран
+    @param  Принимает на вход ссылку на строку text,и ссылку на поток вывода out
+*/
 void print (const std::string& text, std::ostream& out = std::cout );
+/** @brief  Вывод текста в файл
+    @param  Принимает на вход ссылку на строку text,и ссылку на поток вывода в файл out
+*/
 void print (const std::string& text, std::ofstream& out);
